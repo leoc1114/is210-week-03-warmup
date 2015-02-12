ca581a89d1bde1e6864b4e9a57f35678502be596
wei chen <wei.chen@spsmail.cuny.edu>
2015-02-12 18:57:01 +0000

    created task 11 & 12

---
+++ task_11.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+"""A simple python file."""
+
+ESCAPE_STRING = '''\\n'"'''
+
+print ESCAPE_STRING
---
+++ task_12.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python
+# _*_ coding: utf-8 _*_
+"""TASK 12"""
+
+import decimal
+import fractions
+
+INTVAL = 1
+FLOATVAL = 0.1
+
+DECVAL = decimal.Decimal('0.1')
+FRACVAL = fractions.Fraction(1, 10)
+
+print INTVAL
+print FLOATVAL
+print DECVAL
+print FRACVAL
