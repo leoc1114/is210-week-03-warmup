e5d7d28a5a144119ca08693a54b61d1b4463e752
wei chen <wei.chen@spsmail.cuny.edu>
2015-02-12 17:57:31 +0000

    Modified task 7 & 8

--- task_07.py
+++ task_07.py
@@ -5,3 +5,7 @@
 import task_06
 
 WORDS = task_06.WORDS
+
+GRANARIES_EXIST = 'granadries' in WORDS
+
+print GRANARIES_EXIST
--- task_08.py
+++ task_08.py
@@ -18,3 +18,9 @@ NERVOUS_AS = """
 
 
 """
+
+NERVOUS_AS = NERVOUS_AS.strip()
+NERVOUS_AS = NERVOUS_AS.rstrip(',').lstrip('/')
+
+
+print NERVOUS_AS
