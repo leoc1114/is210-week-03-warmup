7897c98395add2233807241db6261cd33ab8e238
wei chen <wei.chen@spsmail.cuny.edu>
2015-02-12 19:57:48 +0000

    added task 14, and modified task 15

---
+++ task_14.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python
+# _*_ coding: utf-8 _*_
+"""Task 14 created"""
+
+
+IS_TRUE = True
+IS_FALSE = False
+IS_NONE = None
+
+INTEGER_EQUIV = IS_TRUE == 1 and IS_FALSE == 0
+
+print IS_TRUE
+print IS_FALSE
+print IS_NONE
+print INTEGER_EQUIV
