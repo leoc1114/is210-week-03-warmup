c781cfc11fb4acdc4e2376b073d1e6988b00ebcd
wei chen <wei.chen@spsmail.cuny.edu>
2015-02-13 17:48:48 +0000

    modified task 9

--- task_09.py
+++ task_09.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question, is a good computer, more importantly
-   if you have a good programmer.
+"""A computer that can build the question, is a good computer.
+More importantly, if you have a good programmer.
 """
 
 
 THE_ANSWER_TO_EVERYTHING = 42
 INFINITE_IMPROBABILITY = 'browning motion'
+
+
+
